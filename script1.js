456d056d6ddeddb6e805ef1d351ceae1884e9e34
Jesús Manuel González <jesusmanuel.gonzalez1@alu.uclm.es>
2018-02-19 22:06:01 +0100

    App CuadernoAlumnos

---
+++ script1.js
@@ -0,0 +1,7 @@
+function AlumnosController($scope){
+	$scope.alumnos=[
+			{nombre: "Jose Antonio", telefono:"123456789", curso: "1º carrera"},
+			{nombre: "Jose ", telefono:"1203456789", curso: "2º carrera"},
+			{nombre: "Antonio", telefono:"1234506789", curso: "3º carrera"}
+	];
+}
