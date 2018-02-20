e5cb435bc87e3f7fe844bbddc7813bc4549f06df
Jesús Manuel González <jesusmanuel.gonzalez1@alu.uclm.es>
2018-02-20 20:08:15 +0100

    Possits

--- script1.js
+++ script1.js
@@ -1,11 +1,11 @@
-function AlumnosController($scope){
-	$scope.alumnos=[
-			{nombre: "Jose Antonio", telefono:"123456789", curso: "1º carrera"},
-			{nombre: "Jose ", telefono:"1203456789", curso: "2º carrera"},
-			{nombre: "Antonio", telefono:"1234506789", curso: "3º carrera"}
+function PossitsController($scope){
+	$scope.possits=[
+			{titulo: "Compra #1", contenido:"Yogurt, Sugar"},
+			{titulo: "Clase #2 ", contenido:"Systems Web Development"},
+			{titulo: "Tarea", contenido:"1234506789"}
 	];
 	$scope.Save=function(){
-	$scope.alumnos.push({nombre:$scope.nuevoAlumno.nombre, telefono: $scope.nuevoAlumno.telefono, curso:$scope.nuevoAlumno.curso});
+	$scope.possits.push({titulo:$scope.nuevoPossit.titulo, contenido: $scope.nuevoPossit.contenido});
 	$scope.formVisibility=false;
 	console.log($scope.formVisibility)
 }//Si lo ponemos fuera da error, puesto que scope está definido en AlumnosController, por ello tiene que estar dentro de la función para poderse ejecutar
