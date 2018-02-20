8eb74fc42f494dce2807f6396d554cf0f8391626
Jesús Manuel González <jesusmanuel.gonzalez1@alu.uclm.es>
2018-02-20 11:51:20 +0100

    Formulario aniadir

--- script1.js
+++ script1.js
@@ -4,4 +4,8 @@ function AlumnosController($scope){
 			{nombre: "Jose ", telefono:"1203456789", curso: "2º carrera"},
 			{nombre: "Antonio", telefono:"1234506789", curso: "3º carrera"}
 	];
+	$scope.Save=function(){
+	$scope.alumnos.push({nombre:$scope.nuevoAlumno.nombre, telefono: $scope.nuevoAlumno.telefono, curso:$scope.nuevoAlumno.curso});
+}//Si lo ponemos fuera da error, puesto que scope está definido en AlumnosController, por ello tiene que estar dentro de la función para poderse ejecutar
+
 }
