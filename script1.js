63d9157a41aa628429b5151f2fe5f605a09a03b9
Carlos Lopez Nieto <carloslopeznieto13@gmail.com>
2018-02-22 11:33:03 +0100

    ya borra los textinput

--- script1.js
+++ script1.js
@@ -1,14 +1,16 @@
-function PossitsController($scope){
+function PossitsController($scope){ //Porque se usa esta funcion general?
 	$scope.possits=[
 			{title: "Market", content:"Yogurt, Sugar"},
 			{title: "Classes", content:"Systems Web Development"},
 			{title: "Homework", content:"Theory Reports"}
 	];
 	$scope.Save=function(){
-	$scope.possits.push({title:$scope.nuevoPossit.title, content: $scope.nuevoPossit.content});
-	$scope.formVisibility=false;
-	console.log($scope.formVisibility)
-}//Si lo ponemos fuera da error, puesto que scope está definido en AlumnosController, por ello tiene que estar dentro de la función para poderse ejecutar
+		$scope.possits.push({title:$scope.nuevoPossit.title, content: $scope.nuevoPossit.content});
+		$scope.formVisibility=false;
+		console.log($scope.formVisibility);
+		$scope.nuevoPossit.title = "";
+		$scope.nuevoPossit.content = "";
+	}//Si lo ponemos fuera da error, puesto que scope está definido en AlumnosController, por ello tiene que estar dentro de la función para poderse ejecutar
 
 $scope.formVisibility=false;
 $scope.ShowForm=function(){
