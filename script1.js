6ce304aa5f1461ee57fd4cf1a18944b164b9b09d
Jesús Manuel González <jesusmanuel.gonzalez1@alu.uclm.es>
2018-03-01 14:07:44 +0100

    Presentation

--- script1.js
+++ script1.js
@@ -1,30 +1,30 @@
 function PossitsController($scope){ 
-	$scope.possits=[
+
+$scope.possits=[
 			{title: "Market", content:"Yogurt, Sugar"},
 			{title: "Classes", content:"Systems Web Development"},
 			{title: "Homework", content:"Theory Reports"}
 	];
-	$scope.Save=function(){
-		$scope.possits.push({title:$scope.nuevoPossit.title, content: $scope.nuevoPossit.content});
-		$scope.formVisibility=false;
-		console.log($scope.formVisibility);
-		$scope.nuevoPossit.title = "";
-		$scope.nuevoPossit.content = "";
-	}
 
-$scope.formVisibility=false;
-$scope.ShowForm=function(){
+	$scope.formVisibility=false;
+	$scope.ShowForm=function(){
 	$scope.formVisibility=true;
 	console.log($scope.formVisibility)
-}
+	}
 
-
-$scope.delete = function (possit) {    
+	$scope.delete = function (possit) {    
 			if (confirm("Do you want delete this Post-it?"))
            {     
 				var delposit = $scope.possits.indexOf(possit);
 				$scope.possits.splice(delposit,1);   
            }            
 }
-
+$scope.Save=function(){
+		$scope.possits.push({title:$scope.nuevoPossit.title, content: $scope.nuevoPossit.content});
+		$scope.formVisibility=false;
+		console.log($scope.formVisibility);
+		$scope.nuevoPossit.title = "";
+		$scope.nuevoPossit.content = "";
+	}
 }
+
